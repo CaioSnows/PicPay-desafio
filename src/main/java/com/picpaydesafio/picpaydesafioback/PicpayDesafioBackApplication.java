fa465730c0c6788f12fd7c66d82ca08945b41436
CaioSnows <caio.cbarrosneves@gmail.com>
2022-10-17 15:21:01 -0300

    Creating a project

---
+++ src/main/java/com/picpaydesafio/picpaydesafioback/PicpayDesafioBackApplication.java
@@ -0,0 +1,13 @@
+package com.picpaydesafio.picpaydesafioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioBackApplication.class, args);
+	}
+
+}
